18fd528e2ddef333f78077d95cfccd74021b31c9
lucaspasti <lucas.pasti@gmail.com>
2023-10-14 19:57:58 -0300

    produto

--- controle/controladorProduto.py
+++ controle/controladorProduto.py
@@ -20,28 +20,52 @@ class ControladorProduto:
 
     def inclui_produto(self):
         dados = self.__tela_produto.pega_dados()  #add parametros
-        produto = Produto(dados['nome'], dados['codigo'], dados['preco'])  # verificar
-        self.__produtos.append(produto)
+        codigo = dados_produto('codigo')
+        produto = self.pega_produto(codigo)
+        try:
+            if produto == None:
+                self.__produtos.append(produto)
+            else:
+                raise ValueError
+        except ValueError:
+            self.__tela_produto.mensagem('Produto já existe')
+
 
     def lista_produtos(self):
-        for p in self.__produtos:
-            self.__tela_produto.mostra({'nome': p.nome, 'codigo': p.codigo, 'preco': p.preco})
+        try:
+            if lista:
+                for p in self.__produtos:
+                    self.__tela_produto.mostra({'nome': p.nome, 'codigo': p.codigo, 'preco': p.preco})
+            else:
+                raise KeyError
+        except KeyError:
+            self.__tela_produto.mensagem('Não há nenhum produto cadastrado.')
 
     def altera_produto(self):
         self.lista_produtos()
         codigo_produto = self.__tela_produto.seleciona()   #add parametros
         produto = self.pega_produto(codigo_produto)
-
-        novos_dados = self.__tela_produto.pega_dados()    #add parametros
-        produto.nome = novos_dados['nome']
-        produto.preco = novos_dados['preco']      # verificar
+        try:
+            if produto:
+                novos_dados = self.__tela_produto.pega_dados()    #add parametros
+                produto.nome = novos_dados['nome']
+                produto.preco = novos_dados['preco']
+            else:
+                raise KeyError
+        except KeyError:
+            self.tela_produto.mensagem('Produto não existente')
 
     def excluir_produto(self):
         self.lista_produtos()
         codigo_produto = self.__tela_produto.seleciona()  # add parametros
         produto = self.pega_produto(codigo_produto)
-
-        self.__produtos.remove(produto)  # verificar
+        try:
+            if produto:
+                self.__produtos.remove(produto)  # verificar
+            else:
+                raise KeyError
+        except KeyError:
+            self.__tela_produto.mensagem('Produto não existente. ')
 
 
     def retorna(self):
